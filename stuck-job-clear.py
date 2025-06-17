ce8f67dc1054040010b02c1f686f6293caa4fc79
Phillip Dykman <phil.d324@gmail.com>
2025-06-17 13:04:55 -0700

    Fix logging and exception reporting

--- stuck-job-clear.py
+++ stuck-job-clear.py
@@ -20,9 +20,16 @@ def main():
         current_time = datetime.now(timezone.utc)
         filter_time = current_time - timedelta(minutes=args.since)
 
+        print("Cleaning up old jobs...")
+
         if args.dry_run:
-            print('Dry run')
-        print(f'Cleaning up jobs older than {args.since} minutes and ...')
+            print(' - Dry run')
+
+        if args.since > 0:
+            print(f' - older than {args.since} minutes')
+
+        if args.method:
+            print(f' - matching method `{args.method}`')
 
         filters = [
             ['state', '=', 'RUNNING'],
@@ -33,20 +40,21 @@ def main():
             filters.append(['method', '=', args.method])
 
         jobs = middleware.call('core.get_jobs', filters)
+        print(f'[+] Found {len(jobs)} total jobs')
 
         for job in jobs:
             start_time = job['time_started']
 
-            # Check if the job started more than 10 minutes ago
+            # Check if the job started more than arg minutes ago
             if args.since == 0 or start_time <= filter_time:
                 job_id = job['id']
                 start_time_formatted = start_time.strftime("%d %B %Y %H:%M:%S")
                 running_time = current_time - start_time
                 running_time_minutes = running_time.total_seconds() // 60.0
 
-                print(f'- Aborting: {job_id}')
-                print(f'- Started: {start_time_formatted}')
-                print(f'- Runtime: {running_time_minutes}m')
+                print(f'Aborting jobId: {job_id}')
+                print(f' - Started: {start_time_formatted}')
+                print(f' - Runtime: {running_time_minutes}m')
 
                 if not args.dry_run:
                     # Abort the job
@@ -56,6 +64,8 @@ def main():
                     else:
                         print(f"-- Failed")
 
+    except Exception as e:
+        print(f"[-] An error occurred: {e}")
     finally:
         # Close the middleware client connection
         middleware.close()
